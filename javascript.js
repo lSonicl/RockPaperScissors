da304df278fcc7d75164009c4689ec491d943c55
l Sonic l <sonicc343@yahoo.com>
2022-12-30 13:38:10 -0800

    player choice function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,22 @@
 getComputerChoice();
+getPlayerChoice();
 
-getComputerChoice()
+function getComputerChoice()
 {
-    let choice = Math.floor(Math.random() * 3) + 1;
-    console.log(choice);
+    const CHOICES = ["ROCK", "PAPER", "SCISSORS"];
+    let choice = Math.floor(Math.random() * 3);
+    console.log(CHOICES[choice]);
+}
+
+function getPlayerChoice()
+{
+    let playerChoice = prompt("Enter Rock, paper, or scissors");
+    playerChoice = playerChoice.toUpperCase();
+    console.log(playerChoice);
+
+    while (playerChoice !== "ROCK" && playerChoice !== "PAPER" && playerChoice !== "SCISSORS")
+    {
+        playerChoice = prompt("Invalid entry. Please enter Rock, paper, or scissors");
+        playerChoice = playerChoice.toUpperCase();
+    }
 }
